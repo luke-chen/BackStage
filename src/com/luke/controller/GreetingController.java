fdaa8c4a819cf28ffcb6d53af9680bd4087a1318
luke <lukechen.lc@gmail.com>
2014-09-23 18:09:30 +0800

    修改注释

--- src/com/luke/controller/GreetingController.java
+++ src/com/luke/controller/GreetingController.java
@@ -8,6 +8,7 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,11 +20,18 @@ import com.luke.model.fruit.Apple;
 import com.luke.model.fruit.Fruit;
 
 @Controller
-@RequestMapping("/spring/*")
+@RequestMapping("/test/*")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    
+    // 导入定义在properties中的属性
+    @Value("#{configProperties['remote.ip']}")
+    String remoteIP;
+    
+    @Value("#{configProperties['remote.port']}")
+    Integer remotePort;
     
     @Autowired
     @Qualifier("orange") // inject instance named "orange" for fruit implement
@@ -42,13 +50,13 @@ public class GreetingController {
     	System.out.println("@PreDestroy run once when this Bean's lifecycle will destory in spring container");
     }
     
-    @ResponseBody
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    @ResponseBody
     public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
     	System.out.println("name:"+name);
     	Greeting greet = new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
-    	System.out.println("id: "+greet.getId()+" data:"+greet.getContent());
+    	System.out.println("greet id: "+greet.getId()+" data:"+greet.getContent());
         return greet;
     }
     
@@ -57,6 +65,7 @@ public class GreetingController {
     public String showJSP(Map<String, Object> model) {
     	System.out.println("fruit-1 name: "+fruit.getName());
     	System.out.println("apple-2 name: "+apple.getName());
+    	System.out.println(String.format("remote IP: %s, port: %s", remoteIP, remotePort));
     	
 //    	model will be used in JSP View
     	model.put("fruit", fruit);
